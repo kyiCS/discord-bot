6ea0b9e9f4870f386046dcc73f0e29dbf62f6dab
Kelvin <kyipersonal@gmail.com>
2022-06-05 20:39:59 -0500

    added test commands

--- bot.py
+++ bot.py
@@ -7,18 +7,25 @@
 
 import os
 
+
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        print(guild)
-    print(f'{client.user} has connected to Discord!')
+class MyClient(discord.Client):
 
+    async def shutdown(ctx):
+        await ctx.bot.logout()
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
 client.run(TOKEN)
